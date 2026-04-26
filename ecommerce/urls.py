ce8e2ece3583ba0424653546d7a970947c198530
Saiteja Golla <tejayadav@gmail.com>
2026-04-26 17:29:31 +0530

    changed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
 
-def home(request):   # 👈 add this
+def home(request):   
     return HttpResponse("Backend is running")
 
 urlpatterns = [
@@ -14,4 +14,5 @@ urlpatterns = [
     path('api/products/', include('apps.products.urls')),
     path('api/cart/', include('apps.cart.urls')),
     path('api/orders/', include('apps.orders.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
