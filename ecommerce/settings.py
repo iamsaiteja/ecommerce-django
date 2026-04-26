b980e1fcc31469b1749385c2db168dfc1a0cccf8
Saiteja Golla <tejayadav@gmail.com>
2026-04-26 19:17:35 +0530

    changed some changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -171,7 +171,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'solemate-media'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 
 # Media files → S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
