cddd6bb7eef0c27880da38f6662e18db825688e2
Saiteja Golla <tejayadav@gmail.com>
2026-04-27 00:26:27 +0530

    remove duplicate s3 settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -178,7 +178,10 @@ AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
-
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+
+
+
