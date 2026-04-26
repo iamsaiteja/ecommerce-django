8ac8fdaeba3626eaf48c4e889924bf184aaab5bd
Saiteja Golla <tejayadav@gmail.com>
2026-04-26 19:12:48 +0530

    Removed duplicate

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -177,7 +177,7 @@ AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
-AWS_DEFAULT_ACL = None
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
